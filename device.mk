d97b28df2b961b9b27e496c560a6e647fd616772
soralis0912 <soralis.nem@gmail.com>
2024-06-06 14:06:11 +0900

    fix codes

--- device.mk
+++ device.mk
@@ -9,8 +9,6 @@ DEVICE_PATH := device/lge/joan
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
 
-# common v30
-$(call inherit-product, device/lge/joan-common/joan.mk)
 
 # Inherit proprietary blobs
 $(call inherit-product, vendor/lge/joan/joan-vendor.mk)
--- extract-files.sh
+++ extract-files.sh
@@ -27,19 +27,12 @@ source "${HELPER}"
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
-ONLY_COMMON=
-ONLY_TARGET=
 KANG=
 SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
-        --only-common )
-                ONLY_COMMON=true
-                ;;
-        --only-target )
-                ONLY_TARGET=true
-                ;;
+
         -n | --no-cleanup )
                 CLEAN_VENDOR=false
                 ;;
@@ -91,20 +84,8 @@ function blob_fixup() {
     esac
 }
 
-if [ -z "${ONLY_TARGET}" ]; then
-    # Initialize the helper for common device
-    setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 
-    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-    extract "${MY_DIR}/proprietary-files_Q910.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-fi
 
-if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" ]; then
-    # Reinitialize the helper for device
-    source "${MY_DIR}/../../${VENDOR}/${DEVICE}/extract-files.sh"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-    extract "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-fi
 
 "${MY_DIR}/setup-makefiles.sh"
