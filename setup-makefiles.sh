56d65b724567e446860d5330de8c3edf769863cf
soralis0912 <soralis.nem@gmail.com>
2024-06-06 14:14:11 +0900

    fix script

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,10 +25,10 @@ fi
 source "${HELPER}"
 
 # Initialize the helper for common
-setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
 # Warning headers and guards
-write_headers "h932 joan"
+write_headers 
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
@@ -37,16 +37,3 @@ write_makefiles "${MY_DIR}/proprietary-files_Q910.txt" true
 # Finish
 write_footers
 
-if [ -s "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" ]; then
-    # Reinitialize the helper for device
-    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false
-
-    # Warning headers and guards
-    write_headers
-
-    # The standard device blobs
-    write_makefiles "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" true
-
-    # Finish
-    write_footers
-fi
